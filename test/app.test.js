284f25a91d961756e406a81a8af70790a4a32f37
kszosze <kszosze@hotmail.com>
2016-04-18 09:23:24 +0100

    Fixing test

---
+++ test/app.test.js
@@ -0,0 +1,9 @@
+var expect = require('chai').expect;
+var bikeasoc = require('../app');
+
+describe('init',function(){
+  it('works',function(done){
+    expect(true).to.be.true;
+    done();
+  });
+});
