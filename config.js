a11bdc0f05a77977c9e0aa148e118e469f51db38
kszosze <kszosze@hotmail.com>
2016-08-20 10:39:25 +0100

    Cleaning up

--- config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    'secret': 'ilovescotchyscotch',
-     db: {
-       test: 'mongodb://127.0.0.1:27017/bykeasoc-test',
-       development: 'mongodb://127.0.0.1:27017/bykeasoc-dev',
-       production: 'mongodb://127.0.0.1:27017/bykeasoc'
-    }
-};
